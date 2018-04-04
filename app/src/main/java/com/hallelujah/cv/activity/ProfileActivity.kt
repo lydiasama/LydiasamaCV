a8b176b403c2727d2c7a077e76c15f9dd9f1581a
Lydiasama <hallelujah.lydia@gmail.com>
2018-04-04 17:22:00 +0700

    new profile activity

---
+++ app/src/main/java/com/hallelujah/cv/activity/ProfileActivity.kt
@@ -0,0 +1,13 @@
+package com.hallelujah.cv.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.hallelujah.cv.R
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
