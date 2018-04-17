896caa68d8639534f3347183687fbcf355bf00ee
Lydiasama <hallelujah.lydia@gmail.com>
2018-04-17 09:32:59 +0700

    add onClickListener

--- app/src/main/java/com/hallelujah/cv/activity/ProfileActivity.kt
+++ app/src/main/java/com/hallelujah/cv/activity/ProfileActivity.kt
@@ -3,11 +3,19 @@ package com.hallelujah.cv.activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.hallelujah.cv.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class ProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        setOnClickProfileAvatar()
+    }
+
+    private fun setOnClickProfileAvatar() {
+        ivAvatar.setOnClickListener {
+            supportFinishAfterTransition()
+        }
     }
 }
